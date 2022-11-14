04b141c681abd1a9d0e7a18e954fb5fa9d6949ef
brown <brown2243@naver.com>
2022-11-14 21:35:38 +0900

    chap.5: 중급개념

--- src/bin/a11.rs
+++ src/bin/a11.rs
@@ -9,4 +9,18 @@
 // * Create a function to display the quantity, with the struct as a parameter
 // * Create a function to display the id number, with the struct as a parameter
 
-fn main() {}
+struct Apple {
+    id: i32,
+    quantity: i32,
+}
+fn display_apple(apple: &Apple) {
+    println!("{}, {}", apple.id, apple.quantity);
+}
+fn main() {
+    let apple = Apple {
+        id: 0,
+        quantity: 9999,
+    };
+    display_apple(&apple);
+    display_apple(&apple);
+}
--- src/bin/a12.rs
+++ src/bin/a12.rs
@@ -9,5 +9,33 @@
 // * Use an enum for the box color
 // * Implement functionality on the box struct to create a new box
 // * Implement functionality on the box struct to print the characteristics
+#[derive(Debug)]
+enum Color {
+    Red,
+    Blue,
+}
+struct Box {
+    dimensions: i32,
+    weight: i32,
+    color: Color,
+}
+impl Box {
+    fn create_box(data: (i32, i32, Color)) -> Self {
+        let (dimensions, weight, color) = data;
+        Self {
+            dimensions,
+            weight,
+            color,
+        }
+    }
+    fn print_characteristics(&self) {
+        println!("{},{},{:?}", self.dimensions, self.weight, self.color);
+    }
+}
 
-fn main() {}
+fn main() {
+    let blue_box = Box::create_box((4, 500, Color::Blue));
+    let red_box = Box::create_box((4, 500, Color::Red));
+    blue_box.print_characteristics();
+    red_box.print_characteristics();
+}
